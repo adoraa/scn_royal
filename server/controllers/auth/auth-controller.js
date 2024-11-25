e41477b4f35f928e3d75137dea2e0e6deacd49a9
Adora <adoraonyile@gmail.com>
2024-11-25 20:00:42 +0100

    implementing backend functionality

---
+++ server/controllers/auth/auth-controller.js
@@ -0,0 +1,47 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
+
+//register
+const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const hashPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({
+      username,
+      email,
+      password: hashPassword,
+    });
+
+    await newUser.save();
+    res.status(200).json({
+      success: true,
+      message: "Registered successfully!",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
+  }
+};
+
+//login
+const loginUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
+  }
+};
+
+
+module.exports = { registerUser };
+// module.exports = { loginUser, logoutUser, authMiddleware };
