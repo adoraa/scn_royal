3f298f59681a8c99b84d5c370f41b6195a1f45c5
Adora <adoraonyile@gmail.com>
2024-11-25 09:23:15 +0100

    added shop view + error page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,12 @@ import AdminDashboard from "./pages/admin-view/dashboard";
 import AdminProducts from "./pages/admin-view/products";
 import AdminOrders from "./pages/admin-view/orders";
 import AdminFeatures from "./pages/admin-view/features";
+import ShoppingLayout from "./components/shop-view/layout";
+import NotFound from "./pages/not-found";
 
 function App() {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
-      <h1>Header</h1>
       <Routes>
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="login" element={<AuthLogin />} />
@@ -23,6 +24,8 @@ function App() {
           <Route path="orders" element={<AdminOrders />} />
           <Route path="features" element={<AdminFeatures />} />
         </Route>
+        <Route path="/shop" element={<ShoppingLayout/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </div>
   );
