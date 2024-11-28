82890cdcd426b1bf681b69a4eb6b74d950a95bc7
Adora <adoraonyile@gmail.com>
2024-11-28 11:03:04 +0100

    admin dashboard updated to crud products

---
+++ server/helpers/cloudinary.js
@@ -0,0 +1,22 @@
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+
+cloudinary.config({
+  cloud_name: "dv8wobz6l",
+  api_key: "297578588665596",
+  api_secret: "p1olEfWegpnlyfH5V7AntjG1IaE",
+});
+
+const storage = new multer.memoryStorage();
+
+async function imageUploadUtil(file) {
+  const result = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+
+  return result;
+}
+
+const upload = multer({ storage });
+
+module.exports = { upload, imageUploadUtil };
