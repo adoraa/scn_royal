8dd60de115beccb8adbae2d33fd182fcb17c31a5
Adora <adoraonyile@gmail.com>
2024-12-07 22:14:13 +0100

    updated links

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,16 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import { Toaster } from "./components/ui/toaster";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
       <Toaster/>
     </Provider>
-  </HashRouter>
+  </BrowserRouter>
 );
