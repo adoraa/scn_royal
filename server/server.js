4fc631390ce4abc85262a676e0f9dbb9bf43bce5
Adora <adoraonyile@gmail.com>
2024-12-09 11:27:27 +0100

    chnaged url

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["https://scn-royal.vercel.app", "http://localhost:5173"],
+    origin: ["https://www.scnroyal.com", "http://localhost:5173"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
