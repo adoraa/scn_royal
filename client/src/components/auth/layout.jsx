fc57ad6fc4f9b3bafbedcd3d382b22759951cffc
Adora <adoraonyile@gmail.com>
2024-12-14 11:13:04 +0100

    added new updates for the login design

--- client/src/components/auth/layout.jsx
+++ client/src/components/auth/layout.jsx
@@ -6,7 +6,7 @@ function AuthLayout() {
     <div className="flex min-h-screen w-full">
       <div className="hidden lg:flex items-center justify-center w-1/2 px-12 relative">
         <img src={logoLight} alt="logo" className="relative z-10" />
-        <div className="absolute right-0 top-1/2 transform -translate-y-1/2 h-1/2 border-r-2 border-[#85c755]"></div>
+        <div className="absolute right-0 top-1/2 transform -translate-y-1/2 h-1/2 border-r-2 border-primary"></div>
       </div>
       <div className="flex flex-1 items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
         <Outlet />
