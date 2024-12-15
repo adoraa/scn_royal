b067ddc41c08111ae71ea5dc16bf66eb612fab70
Adora <adoraonyile@gmail.com>
2024-12-15 11:04:30 +0100

    adjusted grid size

--- client/src/pages/shop-view/home.jsx
+++ client/src/pages/shop-view/home.jsx
@@ -161,7 +161,7 @@ function ShoppingHome() {
           <h2 className="text-3xl font-bold text-center mb-8 font-primary">
             Shop by category
           </h2>
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
             {categoriesWithIcon.map((categoryItem) => (
               <Card
                 onClick={() =>
