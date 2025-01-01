ce2ca88e5d45edc8c8bfcf936bfcf6e49dd4ee81
Adora <adoraonyile@gmail.com>
2025-01-01 18:02:50 +0100

    prettier refactor

--- client/src/pages/shop-view/home.jsx
+++ client/src/pages/shop-view/home.jsx
@@ -72,7 +72,7 @@ function ShoppingHome() {
   }
 
   function handleAddtoCart(getCurrentProductId) {
-    const guestId = 'guest';
+    const guestId = "guest";
     const currentUserId = user?.id || guestId;
     dispatch(
       addToCart({
@@ -183,7 +183,9 @@ function ShoppingHome() {
 
       <section className="py-12 bg-gray-50">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold text-center mb-8 font-primary">Product Category</h2>
+          <h2 className="text-3xl font-bold text-center mb-8 font-primary">
+            Product Category
+          </h2>
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
             {brandsWithIcon.map((brandItem) => (
               <Card
@@ -192,7 +194,9 @@ function ShoppingHome() {
               >
                 <CardContent className="flex flex-col items-center justify-center p-6">
                   <brandItem.icon className="w-12 h-12 mb-4 text-primary" />
-                  <span className="font-bold text-nowrap">{brandItem.label}</span>
+                  <span className="font-bold text-nowrap">
+                    {brandItem.label}
+                  </span>
                 </CardContent>
               </Card>
             ))}
