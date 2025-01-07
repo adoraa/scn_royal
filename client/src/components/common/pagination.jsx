0b5f5cda438183478399e7e27ba45adaa8c221ee
Adora <adoraonyile@gmail.com>
2025-01-07 13:31:50 +0100

    updated ellipsis logic for only 3 numbers

--- client/src/components/common/pagination.jsx
+++ client/src/components/common/pagination.jsx
@@ -12,15 +12,15 @@ import React from "react";
 function PaginationSection({ currentPage, totalPages, onPageChange }) {
   const getPageNumbers = () => {
     const pageNumbers = [];
-    if (totalPages <= 3) {
+    if (totalPages < 4) {
       for (let i = 1; i <= totalPages; i++) {
         pageNumbers.push(i);
       }
     } else {
-      if (currentPage <= 3) {
-        pageNumbers.push(1, 2, 3, '...', totalPages);
+      if (currentPage < 4) {
+        pageNumbers.push(1, 2, 3, "...", totalPages);
       } else if (currentPage > 3 && currentPage < totalPages - 1) {
-        pageNumbers.push(1, currentPage - 1, currentPage, currentPage + 1, '...', totalPages);
+        pageNumbers.push(1, "...", currentPage, currentPage + 1, totalPages);
       } else {
         pageNumbers.push(1, "...", totalPages - 2, totalPages - 1, totalPages);
       }
@@ -44,7 +44,7 @@ function PaginationSection({ currentPage, totalPages, onPageChange }) {
           <React.Fragment key={index}>
             {number === 1 ||
             number === totalPages ||
-            Math.abs(currentPage - number) <= 1 ? (
+            Math.abs(currentPage - number) <= 3 ? (
               <PaginationItem isCurrent={currentPage === number}>
                 {currentPage === number ? (
                   <PaginationLink
